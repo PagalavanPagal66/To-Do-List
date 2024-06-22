4b9afd00b98461cfec056d9acf8471a30b7352a7
Pagalavan <125946240+PagalavanPagal66@users.noreply.github.com>
2024-06-22 12:22:16 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import ToDoList from './Components/ToDoList'
+function App() {
+
+  return (
+    <>
+      <ToDoList></ToDoList>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/ToDoList.tsx
@@ -0,0 +1,92 @@
+import { useState } from "react";
+import './ToDoList.css'
+
+const ToDoList = () => {
+
+    const [taskList,setTaskList] = useState<{task:string,isdone:boolean}[]>([]);
+    const [task,setTask] = useState("");
+    
+    const addtask = () => {
+        const newtask : string = task;
+        const newstate : boolean = false;
+        const newobject = 
+        {
+            task : newtask,
+            isdone : newstate
+        }  
+        setTaskList([...taskList,newobject]);
+        setTask("");
+    }
+    
+    const updatetask = (e : React.ChangeEvent<HTMLInputElement>) => {
+        setTask(e.target.value);
+    }
+
+    const deletetask = (index : number) => {
+        setTaskList(taskList.filter(
+            (element :{task:string , isdone:boolean} ,i : number) => i!==index
+        ))
+    }
+
+    const markdone = (index : number) => {
+        let newTaskList : {task : string, isdone:boolean}[] = []
+        let i : number = 0;
+        for(let iterTask of taskList){
+            if(index!==i){
+                newTaskList.push(
+                    {"task" : iterTask.task  ,
+                    "isdone" : iterTask.isdone}
+                )
+            }
+            else{
+                newTaskList.push(
+                    {"task" : iterTask.task,
+                    "isdone" : true}
+                ) 
+            }
+            i++;
+        }
+        setTaskList(newTaskList);
+    }   
+
+    const markundone = (index : number) => {
+        let newTaskList : {task : string, isdone:boolean}[] = []
+        let i : number = 0;
+        for(let iterTask of taskList){
+            if(index!==i){
+                newTaskList.push(
+                    {"task" : iterTask.task  ,
+                    "isdone" : iterTask.isdone}
+                )
+            }
+            else{
+                newTaskList.push(
+                    {"task" : iterTask.task,
+                    "isdone" : false}
+                ) 
+            }
+            i++;
+        }
+        setTaskList(newTaskList);
+    }   
+
+    return(
+        <div className = "main-div">
+            <input className="input" type="text" value = {task} placeholder="Enter task" onChange={updatetask}/>
+            <button onClick={addtask}>Submit</button>
+            <h1>{task}</h1>
+            <ol className="order-list">
+                {taskList.map(
+                    (currTask :{task:string , isdone:boolean} ,index : number) =>    
+                            <li className = {currTask.isdone ? "doneclass" : "undoneclass"} key={index} id={index.toString()}>
+                                <span>{currTask.task}</span>
+                                <button  onClick={ () => deletetask(index)}>Delete</button>
+                                <button  onClick = { () => markdone(index)}>Done</button>
+                                <button  onClick={ () => markundone(index)}>Undone</button>
+                            </li>
+                )}
+            </ol>   
+        </div>
+    )
+}
+export default ToDoList;
